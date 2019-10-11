e6edc876111fe08d0c5805916fd6303dc61b6389
Matthew Toliusis <matttoliusis1@gmail.com>
2019-10-11 01:44:52 +0000

    Added delete button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(article_params)
+    redirect_to article_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
+
   private
 
   def article_params
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,3 @@
 class Article < ApplicationRecord
-  belongs_to :game
-  has_many :photos
   belongs_to :user
 end
